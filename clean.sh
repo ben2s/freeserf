ca6bd350dc2b38b19e3e442e48eb7dc72648dd80
ben2s <ben2s@telenet.be>
2012-09-22 16:58:17 +0200

    add pushd/popd to clean script

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pushd `dirname $0` >/dev/null
 [ -e Makefile ] && make clean
 [ -e Makefile ] && make distclean
 for f in `cat .gitignore`; do [ $f != SPAE.PA ] && rm -rvf $f; done
+popd
