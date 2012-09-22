160f1d510d6f4637f7962bff9810de03de015e30
ben2s <ben2s@telenet.be>
2012-09-22 17:16:46 +0200

    clean script: no need to clutter the output with directories

--- clean.sh
+++ clean.sh
@@ -3,4 +3,4 @@ pushd `dirname $0` >/dev/null
 [ -e Makefile ] && make clean
 [ -e Makefile ] && make distclean
 for f in `cat .gitignore`; do [ $f != SPAE.PA ] && rm -rvf $f; done
-popd
+popd >/dev/null
